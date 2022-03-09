444f6814f346b82510f948f4ea99ec1a321f8baa
Fernando Calmet <fercalmet@gmail.com>
2022-03-08 20:25:23 -0500

    first commit

---
+++ WebAPI.JSON.CRUD/Helpers/JsonManager.cs
@@ -0,0 +1,198 @@
+﻿namespace WebAPI.JSON.CRUD.Helpers;
+
+using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+public static class JsonManager
+{
+    public static string jsonFile;
+    public static string jsonLocation;
+    private static JObject _dbObject;
+
+    public static JObject Load(string filePathLoaded)
+    {
+        if (!File.Exists(filePathLoaded))
+            throw new Exception("File does not exist, please check file or file path");
+        try
+        {
+            jsonFile = File.ReadAllText(filePathLoaded);
+            jsonLocation = filePathLoaded;
+            return _dbObject = JObject.Parse(jsonFile);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message.ToString());
+        }
+    }
+
+    public static object Select(this object data, string table = "")
+    {
+
+        if (data != null)
+        {
+            try
+            {
+                var parsedData = JObject.Parse(data.ToString());
+                var result = string.IsNullOrEmpty(table) ? (object)parsedData : (object)parsedData[table];
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+    }
+
+    public static List<dynamic> Where(this object data, string key = "", dynamic value = null)
+    {
+        if (data != null)
+        {
+            var dataArray = data.ToDataList();
+            var newDataArray = new List<dynamic>();
+            foreach (var singleData in dataArray.Where(x => x[key] as dynamic == value))
+            {
+                newDataArray.Add(singleData);
+            }
+            return newDataArray;
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+
+    }
+
+    public static void Add(this object data, string table, object newData)
+    {
+        if (data != null)
+        {
+            try
+            {
+                var parsedData = JObject.Parse(data.ToString());
+                var result = string.IsNullOrEmpty(table) ? (object)parsedData : (object)parsedData[table];
+                var dataArray = result.ToDataList();
+                dataArray.Add(JObject.Parse(JsonConvert.SerializeObject(newData, Formatting.Indented)));
+                parsedData[table] = dataArray;
+                string newJsonResult = parsedData.ToString();
+                File.WriteAllText(jsonLocation, newJsonResult);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+    }
+
+    public static void Delete(this object data, string table, string key, dynamic value)
+    {
+        if (data != null)
+        {
+            try
+            {
+                var parsedData = JObject.Parse(data.ToString());
+                var result = (object)parsedData[table];
+                var dataArray = result.ToDataList();
+                var itemToBeDeleted = dataArray.FirstOrDefault(x => x[key] as dynamic == value);
+                if (itemToBeDeleted == null)
+                    throw new Exception("There are no objects with key: " + key + " , Please check key again");
+                dataArray.Remove(itemToBeDeleted);
+                parsedData[table] = dataArray;
+                string newJsonResult = parsedData.ToString();
+                File.WriteAllText(jsonLocation, newJsonResult);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+    }
+
+    public static object Update(this object data, string table, string key, dynamic value, object newData)
+    {
+        if (data != null)
+        {
+            try
+            {
+                var parsedData = JObject.Parse(data.ToString());
+                var result = (object)parsedData[table];
+                var dataArray = result.ToDataList();
+                var x = dataArray.FirstOrDefault(x => x[key] as dynamic == value);
+                if (x == null)
+                    throw new Exception("There are no objects with key: " + key + " , Please check key again");
+                foreach (PropertyInfo prop in newData.GetType().GetProperties())
+                {
+                    var valueObject = prop.GetValue(newData, null);
+                    x[prop.Name] = JToken.FromObject(valueObject);
+                }
+                parsedData[table] = dataArray;
+                string newJsonResult = parsedData.ToString();
+                File.WriteAllText(jsonLocation, newJsonResult);
+                return x;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+    }
+
+    public static string ToJsonString(this object data)
+    {
+        if (data != null)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+    }
+
+    public static JArray ToDataList(this object data)
+    {
+        if (data != null)
+        {
+            try
+            {
+                var token = JToken.Parse(data.ToString());
+                if (token is JArray)
+                {
+                    JArray dataArray = (JArray)data;
+                    return dataArray;
+                }
+                else
+                    throw new Exception("The preloaded data is not a Json Array");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+        else
+        {
+            throw new Exception("The preloaded data is null");
+        }
+    }
+}
---
+++ WebAPI.JSON.CRUD/Program.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json.Linq;
+using WebAPI.JSON.CRUD.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+string fileName = "users.json";
+string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+var dbUsers = Database(path);
+
+app.MapGet("/", () => "Hello World! 🔥");
+
+app.MapGet("/users", () =>
+{
+    dbUsers.Select("users").ToJsonString();
+})
+.WithName("GetUsers");
+
+app.MapGet("/users/{id}", (int id) =>
+{
+    dbUsers.Select("users").Where("id", id).ToJsonString();
+})
+.WithName("GetUserById");
+
+app.MapPost("/users", (User user) =>
+{
+    dbUsers.Add("users", user);
+})
+.WithName("AddUser");
+
+app.MapDelete("/users/{id}", (int id) =>
+{
+    dbUsers.Delete("users", Convert.ToString(id), 1);
+})
+.WithName("RemoveUser");
+
+app.MapPut("/users/{id}", (int id, User user) =>
+{
+    dbUsers.Update("users", Convert.ToString(id), 3, user);
+})
+.WithName("EditUser");
+
+app.Run();
+
+JObject Database(string filePath)
+{
+    return JsonManager.Load(filePath);
+}
+
+internal record User()
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public bool Verified { get; set; }
+}
